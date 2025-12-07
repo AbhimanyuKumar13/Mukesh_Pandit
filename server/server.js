2c0af8ee1d9dc5ce665b19e8127d5cc19e14ed06
AbhimanyuKumar13 <ak6547765@gmail.com>
2025-12-07 17:01:47 +0530

    error resolve

--- server/server.js
+++ server/server.js
@@ -7,11 +7,9 @@ dotenv.config();
 
 const app = express();
  
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:3000', 
-];
- 
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",").map(origin => origin.trim())
+  : [];
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
